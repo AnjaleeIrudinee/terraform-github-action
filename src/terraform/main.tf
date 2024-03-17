420b29e6a08b076dfc548c567c27eff4ed1c4cca
Anjalee Irudinee <anjaleeirudinee9396@gmail.com>
2024-03-17 22:45:02 +0530

    added terraform block new

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
   # Update this block with the location of your terraform state file
   backend "azurerm" {
     resource_group_name  = "anjigithubdeploy"
-    storage_account_name = "tteststoreanji01"
+    storage_account_name = "teststoreanji01"
     container_name       = "tfstate"
     key                  = "demo.terraform.tfstate"
    
